7af9c80eafb94f7a26caa620499548e99333941e
CodedPro <dev.codedpro@gmail.com>
2024-06-11 11:15:18 +0330

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useEffect, useRef } from "react";
 import HeroTitle from "./Hero/HeroTitle";
 import HeroSubtitle from "./Hero/HeroSubtitle";
 import HeroButton from "./Hero/HeroButton";
@@ -16,16 +17,25 @@ const Hero: React.FC<HeroProps> = ({
   buttonText,
   buttonLink,
 }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(err => console.log(err));
+    }
+  }, []);
 
   return (
     <section className="relative overflow-hidden">
       <video
+        ref={videoRef}
         src="/gif/0e965785cf52d3a8844ed338dae79028-3.mp4"
         width={1000}
         height={1000}
         autoPlay
         muted
         loop
+        playsInline
         className="fixed top-0 left-0 w-full h-full object-cover bg-cover bg-no-repeat"
       />
       <div className="relative z-20 flex flex-col items-center justify-center h-screen">
